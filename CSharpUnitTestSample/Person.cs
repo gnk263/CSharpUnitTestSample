bd78048bfac7977f2668f65de159f221a9f9b5ce
gnk263 <gnk263@users.noreply.github.com>
2017-12-21 00:15:05 +0900

    Personクラスを実装

---
+++ CSharpUnitTestSample/Person.cs
@@ -0,0 +1,36 @@
+﻿namespace CSharpUnitTestSample
+{
+    public class Person
+    {
+        private string _firstName = "Yamada";
+        private string _lastName = "Takeshi";
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                if (!string.IsNullOrEmpty(value) && !string.IsNullOrWhiteSpace(value))
+                    _firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                if (!string.IsNullOrEmpty(value) && !string.IsNullOrWhiteSpace(value))
+                    _lastName = value;
+            }
+        }
+
+        public string FullName() => $"{FirstName} {LastName}";
+    }
+}
